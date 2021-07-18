d79a54bb57603f44c091ce31730642c5aaa81acb
Brandon Lamb <blamb888@gmail.com>
2021-07-18 16:13:13 +0900

    try documentation function

--- handler.js
+++ handler.js
@@ -1,28 +1,44 @@
 'use strict';
 require('dotenv').config();
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "ap-northeast-1" });
 
-const {
-  sendEmailToCustomer
-} = require('./ses');
+// const {
+//   sendEmailToCustomer
+// } = require('./ses');
 
 module.exports.simpleEmail = async (event) => {
-  console.log("Sending test email...");
-  const simpleService = sendEmailToCustomer('brandon@flagship.cc');
-  console.log("Test email sent.");
+  // console.log("Sending test email...");
+  // const simpleService = sendEmailToCustomer('brandon@flagship.cc');
+  // console.log("Test email sent.");
 
 
-  console.log("Simple service ran and returned this: " + simpleService);
-  console.log("This was the event: " + event);
+  // console.log("Simple service ran and returned this: " + simpleService);
+  // console.log("This was the event: " + event);
 
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(
-      {
-        message: 'Hopefully this sent an email',
-      }, null,2
-    ),
+  // return {
+  //   statusCode: 200,
+  //   headers: { "Access-Control-Allow-Origin": "*" },
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'Hopefully this sent an email',
+  //     }, null,2
+  //   ),
+  // };
+  var params = {
+    Destination: {
+      ToAddresses: ['brandon@flagship.cc'],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "Test" },
+      },
+      Subject: { Data: "Test Email" },
+    },
+    Source: "blamb888@gmail.com",
   };
+
+  return ses.sendEmail(params).promise()
 };
 
 // module.exports.roundTwo = async (event) => {

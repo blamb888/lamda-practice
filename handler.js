6ba5e17a78ace997f48ee0c6d5d40d9544794c72
Brandon Lamb <blamb888@gmail.com>
2021-07-18 16:49:24 +0900

    update handler to accept docuFunction

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const {
   sendEmailToCustomer
-} = require('./ses');
+} = require('./docuFunction');
 
 module.exports.simpleEmail = async (event) => {
   console.log("Sending test email...");

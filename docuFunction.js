5fb98ea3f4a58736a4efe7b6773546a5d5099999
Brandon Lamb <blamb888@gmail.com>
2021-07-19 12:53:06 +0900

    add logs

--- docuFunction.js
+++ docuFunction.js
@@ -9,6 +9,7 @@ const {
 
 const sendEmailToCustomer = async(customer_email) => {
   const FROM_EMAIL = process.env.SES_FROM_EMAIL_ADDRESS;
+  console.log(FROM_EMAIL);
   const EMAIL_TEMPLATE = emailTemplate();
   const SUBJECT = 'メール配信停止のご依頼を承りました';
   const CHARSET = 'UTF-8';

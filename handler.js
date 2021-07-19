afae33bbfa0532d58974003bd27118c3442a88c3
Brandon Lamb <blamb888@gmail.com>
2021-07-19 12:58:54 +0900

    add logs

--- handler.js
+++ handler.js
@@ -7,6 +7,7 @@ const {
 
 module.exports.simpleEmail = async (event) => {
   console.log("Sending test email...");
+  console.log(FROM_EMAIL);
   const email = await sendEmailToCustomer('brandon@flagship.cc')
 
   return {
---
+++ ses2.js
@@ -0,0 +1,55 @@
+equire('dotenv').config();
+const AWS = require('aws-sdk');
+const SES = new AWS.SES({apiVersion: '2010-12-01'});
+
+// Be sure to change this variables value once testing is done
+// const SES_FROM_EMAIL_ADDRESS = process.env.SES_FROM_EMAIL_ADDRESS;
+const SES_FROM_EMAIL_ADDRESS = "brandon+from@flagship.cc";
+
+function sendEmailToCustomer(customer_email) {
+
+    console.log("This is the FROM email: " + SES_FROM_EMAIL_ADDRESS);
+    console.log("This is the TO email: " + customer_email);
+
+    const emailParams = {
+        Source: SES_FROM_EMAIL_ADDRESS,
+        ReplyToAddresses: [SES_FROM_EMAIL_ADDRESS],
+        Destination: {
+          ToAddresses: [customer_email],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: 'UTF-8',
+              Data: `<h1>Thanks for your message:</h1> ${customer_email}\n\n
+              <h3>I will reply as soon as possible\n Cheers, \n -- Brandon</h3>
+              ${email_template}`,
+              // TODO Insert HTML for pages/unsubscribe-success.liquid here ^
+            },
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: 'メール配信停止のご依頼を承りました',
+          },
+        },
+    };
+
+    console.log(emailParams)
+
+    const sendPromise =  SES.sendEmail(emailParams).promise();
+    console.log(sendPromise);
+
+    sendPromise.then(
+      function(data) {
+        console.log(data.MessageId);
+      }).catch(
+        function(err) {
+        console.error(err, err.stack);
+      });
+}
+
+module.exports = {
+  sendEmailToCustomer
+}
+
+const email_template = "<h1>Hello there!</h1>";
